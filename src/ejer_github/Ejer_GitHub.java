dfecf0259980a1b2068e69a6b0303d5d1d8548f5
Juana Garcia Seañez <jjggsscia@gmail.com>
2021-04-19 12:43:41 -0600

    Programa despliega saludos, primer ejercicio

---
+++ src/ejer_github/Ejer_GitHub.java
@@ -0,0 +1,19 @@
+/*
+ Ejercicios para visualizar en Git Hub
+ */
+package ejer_github;
+
+/**
+ *
+ * @author jjggs_000
+ */
+public class Ejer_GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hola grupo de POO");
+    }
+    
+}
